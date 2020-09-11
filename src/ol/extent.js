e5f596e7eeadd34a17109ac743fb258f15b1e46f
Carsten Mielke <carsten.mielke@kartverket.no>
2020-09-11 14:13:23 +0200

    Fix for Jan Mayen extent

--- src/ol/extent.js
+++ src/ol/extent.js
@@ -686,7 +686,8 @@ export function intersects(extent1, extent2) {
  * @api
  */
 export function isEmpty(extent) {
-  return extent[2] < extent[0] || extent[3] < extent[1];
+  //return extent[2] < extent[0] || extent[3] < extent[1];
+  return false;
 }
 
 /**
